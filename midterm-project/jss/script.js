4221b092435c12cb68b4a4442d2e2ea6f712ad4f
Connor Lafferty <connor.lafferty@uconn.edu>
2026-03-10 10:38:28 -0400

    midterm project

---
+++ midterm-project/jss/script.js
@@ -0,0 +1,7 @@
+var button1 = document.querySelector("#KBTN")
+button1.addEventListener("click", function(){
+    var hero = document.querySelector("#hero1");
+    var button = document.querySelector("#TBTN");
+    var container = document.querySelector(".container");
+    //box.classList.remove("keyframes-page");
+})
