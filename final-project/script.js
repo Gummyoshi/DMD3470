f4006b6eb6200377293b67ede712191ddbaa7402
Connor Lafferty <gummyoshi@gmail.com>
2026-04-30 10:31:05 -0400

    final build

--- final-project/script.js
+++ final-project/script.js
@@ -18,14 +18,14 @@ const playButton = document.getElementById("playButton");
 const tutorialBox = document.querySelector(".tutorial-box");
 
 playButton.addEventListener("click", () => {
+  tutorialBox.classList.add("is-playing");
+
   tutorialBox.innerHTML = `
     <iframe
       src="https://www.youtube.com/embed/ZILH781tceE?autoplay=1"
       title="YouTube video player"
-      frameborder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowfullscreen
-      style="position:absolute; inset:0; width:100%; height:100%; border:0;">
+      allowfullscreen>
     </iframe>
   `;
 });
